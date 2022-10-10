5a23e10a78090bea83a9f6a784d68a830aef6667
Miguel Abache <miiguel.abache@gmail.com>
2022-10-10 19:46:55 -0400

    Subiendo proyecto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp, Vue } from "vue";
+import App from "./App.vue";
+import router from "./router";
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
-createApp(App).use(router).mount('#app')
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+createApp(App).use(router, VueAxios, axios).mount("#app");
